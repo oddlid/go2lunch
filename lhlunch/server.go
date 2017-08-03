bb6ab1734f9825430b46820e735fe45a4c0cde35
Odd E. Ebbesen <odd.ebbesen@wirelesscar.com>
2017-08-03 16:59:10 +0200

    Added handling of different output formats

--- lhlunch/server.go
+++ lhlunch/server.go
@@ -1,38 +1,96 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/oddlid/go2lunch/site"
-	"html/template"
+	htmpl "html/template"
 	"net/http"
+	ttmpl "text/template"
 )
 
 const (
-	lhtmpl  string = "lhlunch.html"
-	tmpl_ID string = "LH"
+	htmpl_ID     string = "LH_HTML"
+	ttmpl_ID     string = "LH_TEXT"
+	urlpath_base string = "/lindholmen"
 )
 
-var tmpl = template.Must(template.New(tmpl_ID).Parse(lhlunch_tmpl_str))
+type lhHandler struct{}
 
-func renderTemplate(w http.ResponseWriter, tpl string, s *site.Site) {
-	err := tmpl.ExecuteTemplate(w, tpl, s)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+var tmpl_html = htmpl.Must(htmpl.New(htmpl_ID).Parse(lhlunch_html_tmpl_str))
+var tmpl_text = ttmpl.Must(ttmpl.New(ttmpl_ID).Parse(lhlunch_text_tmpl_str))
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
+func setupMux() {
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/"] = lhHandlerHTML
+	mux[urlpath_base+".html"] = lhHandlerHTML
+	mux[urlpath_base+".txt"] = lhHandlerTXT
+	mux[urlpath_base+".json"] = lhHandlerJSON
 }
 
-func lhHandler(w http.ResponseWriter, r *http.Request) {
+func (*lhHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := mux[r.URL.String()]; ok {
+		h(w, r) // pass on to registered handler
+		return
+	}
+	fmt.Fprintf(w, "LHLunch server: %q", r.URL.String())
+}
+
+func initSite(w http.ResponseWriter) error {
 	if _site == nil {
 		http.Error(w, "Site is uninitialised", http.StatusInternalServerError)
-		return
+		return fmt.Errorf("Site not initialised")
 	}
 	if _site.s.Restaurants == nil {
 		log.Debug("No content yet, scraping...")
 		err := update()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			return err
 		}
 	}
-	renderTemplate(w, tmpl_ID, _site.s)
+	return nil
+}
+
+func renderHTMLTemplate(w http.ResponseWriter, tpl string, s *site.Site) {
+	err := tmpl_html.ExecuteTemplate(w, tpl, s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func renderTextTemplate(w http.ResponseWriter, tpl string, s *site.Site) {
+	err := tmpl_text.ExecuteTemplate(w, tpl, s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func lhHandlerHTML(w http.ResponseWriter, r *http.Request) {
+	err := initSite(w)
+	if err != nil {
+		return
+	}
+	renderHTMLTemplate(w, htmpl_ID, _site.s)
+}
+
+func lhHandlerTXT(w http.ResponseWriter, r *http.Request) {
+	err := initSite(w)
+	if err != nil {
+		return
+	}
+	renderTextTemplate(w, ttmpl_ID, _site.s)
+}
+
+func lhHandlerJSON(w http.ResponseWriter, r *http.Request) {
+	err := initSite(w)
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = _site.s.Encode(w)
+	if err != nil {
+		log.Errorf("Error serving JSON: %q", err.Error())
+	}
 }
