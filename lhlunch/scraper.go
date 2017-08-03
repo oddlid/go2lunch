b7827fe918911c79c94696375839c2d971ea3fbe
Odd E. Ebbesen <odd.ebbesen@wirelesscar.com>
2017-08-04 00:03:58 +0200

    Added getRestaurantUrl to get proper link to each restaurant

--- lhlunch/scraper.go
+++ lhlunch/scraper.go
@@ -8,6 +8,17 @@ import (
 	"time"
 )
 
+// getRestaurantUrl is a helper that tries to return the link to each restaurant
+func getRestaurantUrl(name string) string {
+	urlBase := "https://www.lindholmen.se/restauranger/"
+	name = strings.Replace(name, "´", "", -1)  // remove forward apostrophe
+	name = strings.Replace(name, "`", "", -1)  // remove backward apostrophe
+	name = strings.Replace(name, " ", "-", -1) // replace space with hyphen
+	name = strings.ToLower(name)
+	name = strings.Replace(name, "ä", "a", -1)
+	return urlBase + name
+}
+
 func scrape(url string) (site.Restaurants, error) {
 	csel := []string{
 		"h3.title",
@@ -32,7 +43,7 @@ func scrape(url string) (site.Restaurants, error) {
 		rname := sel1.Find("a").Text()
 		//log.Debugf("Found restaurant: %q", rname)
 
-		r := &site.Restaurant{Name: rname, Parsed: time.Now(), Url: url}
+		r := &site.Restaurant{Name: rname, Parsed: time.Now(), Url: getRestaurantUrl(rname)}
 		num_restaurants++
 
 		sel1.NextFilteredUntil(csel[1], csel[0]).Each(func(j int, sel2 *goquery.Selection) {
