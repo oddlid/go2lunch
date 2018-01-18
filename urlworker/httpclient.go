d2f44e9c6684aba9269ddc1c82ee409c945f2dfe
Odd E. Ebbesen <odd@oddware.net>
2018-01-18 01:41:21 +0100

    Dummy commit

--- urlworker/httpclient.go
+++ urlworker/httpclient.go
@@ -3,9 +3,12 @@ package urlworker
 
 import (
 	//"crypto/tls"
-	//log "github.com/Sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 	"net/http"
 	"time"
+	"io"
+	"io/ioutil"
+	"bytes"
 )
 
 const (
@@ -23,20 +26,29 @@ func NewHttpClient() *HttpClient {
 		Client: http.DefaulClient,
 		UA:     DEF_UA,
 	}
-
-	return client
+	return client.SetTimeout(DEF_TMOUT)
 }
 
-func (c *HttpClient) SkipCertCheck(skip bool) {
+func (c *HttpClient) SkipCertCheck(skip bool) *HttpClient {
 	c.Transport.TLSClientConfig.InsecureSkipVerify = skip
+	return c
 }
 
-func (c *HttpClient) SetKeepAlives(val bool) {
+func (c *HttpClient) SetKeepAlives(val bool) *HttpClient {
 	c.Transport.DisableKeepAlives = val
+	return c
 }
 
-func (c *HttpClient) SetTimeout(sec float64) {
+func (c *HttpClient) SetTimeout(sec float64) *HttpClient {
 	c.Client.Timeout = time.Second * time.Duration(sec)
+	return c
+}
+
+func (c *HttpClient) Setup(ua string, timeout float64, keepAlive, skipcertcheck bool) *HttpClient {
+	if ua != "" {
+		c.UA = ua
+	}
+	return c.SetTimeout(timeout).SetKeepAlives(keepAlive).SkipCertCheck(skipcertcheck)
 }
 
 func (c *HttpClient) Get(url) (*http.Response, error) {
@@ -48,3 +60,33 @@ func (c *HttpClient) Get(url) (*http.Response, error) {
 
 	return c.Do(req)
 }
+
+// GetResponseBody reads http.Response.Body, closes it, and returns the contents as a byte slice
+func GetResponseBody(res *http.Response) ([]byte, error) {
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func GetResponseReader(res *http.Response) bytes.Reader {
+	b, err := GetResponseBody(res)
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+	return bytes.NewReader(b)
+}
+
+func CloseResponse(res *http.Response) error {
+	// See https://stackoverflow.com/questions/17948827/reusing-http-connections-in-golang
+	io.Copy(ioutil.Discard, res.Body)
+	return res.Body.Close()
+}
+
--- urlworker/urlworker.go
+++ urlworker/urlworker.go
@@ -95,6 +95,31 @@ func Get(done <-chan struct{}, in <-chan Request) <-chan Response {
 	return out
 }
 
+func Get2(done <-chan struct{}, in <-chan Request) <-chan Response {
+	out := make(chan Response)
+
+	go func() {
+		defer close(out)
+		hc := NewHttpClient()
+		// configure the client here if needed
+		for req := range in {
+			res, err := hc.Get(req.Url)
+			if err != nil {
+				log.Errorf("Error fetching URL: %s", err)
+				return
+			}
+
+			select {
+			case out <- Response{res, req}:
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	return out
+}
+
 func Merge(done <-chan struct{}, cs ...<-chan Response) <-chan Response {
 	var wg sync.WaitGroup
 	out := make(chan Response)
@@ -127,7 +152,7 @@ func InitWithWorkers(workers int, done <-chan struct{}, reqs ...Request) <-chan 
 	in := Seed(done, reqs...)
 	minions := make([]<-chan Response, workers)
 	for i := 0; i < workers; i++ {
-		minions[i] = Get(done, in)
+		minions[i] = Get2(done, in)
 	}
 	return Merge(done, minions...)
 }
