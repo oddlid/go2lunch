0c26df11d08b4bcd12de26e5b9dde91a1c26ae5d
Odd E. Ebbesen <odd@oddware.net>
2018-10-02 01:20:40 +0200

    Added Google Analytics to lhlunch_tmpl.go

--- lhlunch/lhlunch_tmpl.go
+++ lhlunch/lhlunch_tmpl.go
@@ -105,6 +105,16 @@ var lhlunch_html_tmpl_str string = `
 			}
 			/*]]>*/
 		</style>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126840341-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-126840341-1');
+		</script>
+
 		<script type="text/javascript">
 			/*<![CDATA[*/
 			var _open = true;
