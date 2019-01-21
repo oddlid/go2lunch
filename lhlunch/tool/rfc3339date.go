00962250732a2b0fca4daf6662782385246107dc
Odd E. Ebbesen <oddebb@gmail.com>
2019-01-22 00:47:19 +0100

    Added tool/rfc3339date.go as hack to get RFC3339 date on OSX

---
+++ lhlunch/tool/rfc3339date.go
@@ -0,0 +1,15 @@
+/*
+This file exists only because ´date´ in OSX can't print
+a proper RFC3339 date, so I made this hack.
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println(time.Now().Format(time.RFC3339))
+}
