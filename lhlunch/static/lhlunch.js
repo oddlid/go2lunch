46179aa1f083ec821e039f22a0b0255a88f981fe
Odd E. Ebbesen <oddebb@gmail.com>
2019-01-31 22:06:52 +0100

    Moved JS to separate file

---
+++ lhlunch/static/lhlunch.js
@@ -0,0 +1,15 @@
+var _open = true;
+function toggledetail() {
+	var ds = document.getElementsByTagName("details");
+	var len = ds.length;
+	for (var i = 0; i < len; i++) {
+		if (_open) {
+			ds[i].removeAttribute("open");
+		}
+		else {
+			ds[i].setAttribute("open", "");
+		}
+	}
+	_open = !_open;
+}
+
