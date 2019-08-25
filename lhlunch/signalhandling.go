2254c165773181d7b2edaa5355a3a9715bfc1de0
Odd E. Ebbesen <oddebb@gmail.com>
2019-08-25 23:03:03 +0200

    Commented out signals not in use anymore

--- lhlunch/signalhandling.go
+++ lhlunch/signalhandling.go
@@ -13,22 +13,37 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-func setupSignalHandling() {
+func setupSignalHandling(quit chan<- bool, numServers int) {
 	sig_chan := make(chan os.Signal, 1)
-	signal.Notify(sig_chan, syscall.SIGUSR1, syscall.SIGUSR2)
+	signal.Notify(sig_chan,
+		syscall.SIGUSR1,
+		syscall.SIGUSR2,
+		syscall.SIGINT,
+		syscall.SIGKILL,
+		syscall.SIGTERM,
+	)
 	go func() {
 		for sig := range sig_chan {
 			switch sig {
-			case syscall.SIGUSR1: // re-scrape and update internal DB
-				err := update()
-				if err != nil {
-					log.Error(err.Error())
-				}
-			case syscall.SIGUSR2: // dump internal DB to stdout
-				log.Info("Dumping parsed contents as JSON to STDOUT:")
-				err := _site.ll.Encode(os.Stdout)
-				if err != nil {
-					log.Error(err.Error())
+//			case syscall.SIGUSR1: // re-scrape and update internal DB
+//				err := update()
+//				if err != nil {
+//					log.Error(err.Error())
+//				}
+//			case syscall.SIGUSR2: // dump internal DB to stdout
+//				log.Info("Dumping parsed contents as JSON to STDOUT:")
+//				err := _site.ll.Encode(os.Stdout)
+//				if err != nil {
+//					log.Error(err.Error())
+//				}
+			case syscall.SIGUSR1, syscall.SIGUSR2:
+				log.Debug("SIGUSR[1|2]: Deprecated signals")
+			case syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM:
+				log.Info("Got quit signal, notifying goroutines...")
+				//close(done)
+				// send signals, one for each goroutine with a server
+				for i := 0; i < numServers; i++ {
+					quit <- true
 				}
 			default:
 				log.Debug("Caught unhandled signal, exiting...")
--- lhlunch/signalhandling_windows.go
+++ lhlunch/signalhandling_windows.go
@@ -4,11 +4,11 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-func setupSignalHandling() {
+func setupSignalHandling(_ chan<- bool, _ int) {
 	log.Info("Skipping setup of signal handling, as we are running on Windows")
 }
 
-func notifyPid(pid int) error {
+func notifyPid(_ int) error {
 	log.Info("notifyPid(): NO-OP as we are running on Windows")
 	return nil
 }
