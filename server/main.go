ffb44b982842130a309d13ececdbc7a80d2ab42f
Odd E. Ebbesen <oddebb@gmail.com>
2021-05-20 21:19:46 +0200

    Updated VERSION

--- server/main.go
+++ server/main.go
@@ -43,7 +43,7 @@ import (
 )
 
 const (
-	VERSION           string = "2021-05-19"
+	VERSION           string = "2021-05-20"
 	DEF_WEB_ADR       string = ":20666"
 	DEF_ADM_ADR       string = ":20667"
 	DEF_COUNTRY_ID    string = "se"
