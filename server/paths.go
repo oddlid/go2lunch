95ae68c2cdc67dafcf3113941c147966d34547b1
Odd Eivind Ebbesen <odd.eivind.ebbesen@ericsson.com>
2022-09-06 16:08:20 +0200

    Updated paths and tests
    Change-Id: Iddc0d5197e2c4fed7f0f432b649a75668a989822

--- server/paths.go
+++ server/paths.go
@@ -1,5 +1,31 @@
 package server
 
+/*
+The URL paths to serve should be these:
+
+/
+/{country}
+/{country}/{city}
+/{country}/{city}/{site}
+/{country}/{city}/{site}/{restaurant}
+/{country}/{city}/{site}/{restaurant}/{dish}
+
+The virtual files to serve should be these:
+
+lunchlist.{html,txt,json}
+country.{html,txt,json}
+city.{html,txt,json}
+site.{html,txt,json}
+restaurant.{html,txt,json}
+dish.{html,txt,json}
+
+It would have been more clear to type all of the above out verbatim where they're used,
+but in order to minimize the risk of typos and subtle bugs, I've gone for this not so
+obvious way of doing it, where I use constants and types to build all of the above
+dynamically on the fly, so that the output would get updated if any constant is changed.
+
+*/
+
 import (
 	"fmt"
 	"strings"
@@ -11,6 +37,7 @@ type outputFormat uint8
 const (
 	slash            = `/`
 	htmlTemplateFile = `allhtml.go.tpl`
+	textTemplateFile = `alltext.go.tpl`
 	lunchList        = `lunchlist`
 	country          = `country`
 	city             = `city`
@@ -20,6 +47,8 @@ const (
 	extHTML          = `html`
 	extTXT           = `txt`
 	extJSON          = `json`
+	pathStatic       = `static`
+	pathTemplates    = `tmpl`
 )
 
 const (
@@ -65,31 +94,33 @@ func (id urlID) fileName(format outputFormat) string {
 	}
 }
 
-func buildRouterPathArgs(elements ...string) []string {
+func buildRouterPathArgs(elements ...any) (string, []any) {
 	var buf strings.Builder
 	for range elements {
 		buf.WriteString(slash)
 		buf.WriteString(`{%s}`)
 	}
-	return append([]string{buf.String()}, elements...)
+	return buf.String(), append([]any{}, elements...)
 }
 
 func (id urlID) routerPath() string {
 	switch id {
 	case idCountry:
-		a := buildRouterPathArgs(country)
-		return fmt.Sprintf(a[0], a[1:])
+		format, args := buildRouterPathArgs(country)
+		return fmt.Sprintf(format, args...)
 	case idCity:
-		a := buildRouterPathArgs(country, city)
-		return fmt.Sprintf(a[0], a[1:])
+		format, args := buildRouterPathArgs(country, city)
+		return fmt.Sprintf(format, args...)
 	case idSite:
-		a := buildRouterPathArgs(country, city, site)
-		return fmt.Sprintf(a[0], a[1:])
+		format, args := buildRouterPathArgs(country, city, site)
+		return fmt.Sprintf(format, args...)
+	case idRestaurant:
+		format, args := buildRouterPathArgs(country, city, site, restaurant)
+		return fmt.Sprintf(format, args...)
+	case idDish:
+		format, args := buildRouterPathArgs(country, city, site, restaurant, dish)
+		return fmt.Sprintf(format, args...)
 	default:
 		return slash
 	}
-}
-
-func surroundWithSlash(word string) string {
-	return slash + word + slash
 }
--- server/paths_test.go
+++ server/paths_test.go
@@ -6,15 +6,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_urlID_fileName(t *testing.T) {
+	assert.Equal(t, "lunchlist.html", idLunchList.fileName(outputHTML))
+	assert.Equal(t, "lunchlist.txt", idLunchList.fileName(outputTXT))
+	assert.Equal(t, "lunchlist.json", idLunchList.fileName(outputJSON))
+
+	assert.Equal(t, "country.html", idCountry.fileName(outputHTML))
+	assert.Equal(t, "country.txt", idCountry.fileName(outputTXT))
+	assert.Equal(t, "country.json", idCountry.fileName(outputJSON))
+
+	assert.Equal(t, "city.html", idCity.fileName(outputHTML))
+	assert.Equal(t, "city.txt", idCity.fileName(outputTXT))
+	assert.Equal(t, "city.json", idCity.fileName(outputJSON))
+
+	assert.Equal(t, "site.html", idSite.fileName(outputHTML))
+	assert.Equal(t, "site.txt", idSite.fileName(outputTXT))
+	assert.Equal(t, "site.json", idSite.fileName(outputJSON))
+
+	assert.Equal(t, "restaurant.html", idRestaurant.fileName(outputHTML))
+	assert.Equal(t, "restaurant.txt", idRestaurant.fileName(outputTXT))
+	assert.Equal(t, "restaurant.json", idRestaurant.fileName(outputJSON))
+
+	assert.Equal(t, "dish.html", idDish.fileName(outputHTML))
+	assert.Equal(t, "dish.txt", idDish.fileName(outputTXT))
+	assert.Equal(t, "dish.json", idDish.fileName(outputJSON))
+}
+
 func Test_buildRouterPathArgs(t *testing.T) {
-	assert.Equal(
-		t,
-		[]string{"/{%s}", "one"},
-		buildRouterPathArgs("one"),
-	)
-	assert.Equal(
-		t,
-		[]string{"/{%s}/{%s}", "one", "two"},
-		buildRouterPathArgs("one", "two"),
-	)
+	format, args := buildRouterPathArgs("one")
+	assert.Equal(t, "/{%s}", format)
+	assert.Len(t, args, 1)
+	assert.Equal(t, "one", args[0])
+
+	format, args = buildRouterPathArgs("one", "two")
+	assert.Equal(t, "/{%s}/{%s}", format)
+	assert.Len(t, args, 2)
+	assert.Equal(t, "one", args[0])
+	assert.Equal(t, "two", args[1])
+}
+
+func Test_urlID_routerPath(t *testing.T) {
+	assert.Equal(t, slash, idLunchList.routerPath())
+	assert.Equal(t, "/{country}", idCountry.routerPath())
+	assert.Equal(t, "/{country}/{city}", idCity.routerPath())
+	assert.Equal(t, "/{country}/{city}/{site}", idSite.routerPath())
+	assert.Equal(t, "/{country}/{city}/{site}/{restaurant}", idRestaurant.routerPath())
+	assert.Equal(t, "/{country}/{city}/{site}/{restaurant}/{dish}", idDish.routerPath())
 }
