8cf9b3020fbb47ba792049a8f86ad33c747d3f2d
Odd E. Ebbesen <oddebb@gmail.com>
2022-09-24 01:37:53 +0200

    Unit tests done for current method set in lunchdata

--- lunchdata/lunchlist.go
+++ lunchdata/lunchlist.go
@@ -17,192 +17,93 @@ func NewLunchList() *LunchList {
 	}
 }
 
-// func (ll *LunchList) Len() int {
-// 	ll.mu.RLock()
-// 	defer ll.mu.RUnlock()
-// 	return len(ll.Countries)
-// }
+func (l *LunchList) NumCountries() int {
+	if l == nil {
+		return 0
+	}
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.Countries.Len()
+}
 
-// func (ll *LunchList) PropagateGtag(tag string) *LunchList {
-// 	ll.mu.Lock()
-// 	ll.Gtag = tag
-// 	for k := range ll.Countries {
-// 		ll.Countries[k].SetGTag(tag)
-// 	}
-// 	ll.mu.Unlock()
-// 	return ll
-// }
+func (l *LunchList) NumCities() int {
+	if l == nil {
+		return 0
+	}
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.Countries.NumCities()
+}
 
-// func (ll *LunchList) AddCountry(c *Country) *LunchList {
-// 	ll.mu.Lock()
-// 	ll.Countries[c.ID] = c
-// 	ll.mu.Unlock()
-// 	return ll
-// }
+func (l *LunchList) NumSites() int {
+	if l == nil {
+		return 0
+	}
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.Countries.NumSites()
+}
 
-// func (ll *LunchList) DeleteCountry(id string) *LunchList {
-// 	ll.mu.Lock()
-// 	delete(ll.Countries, id)
-// 	ll.mu.Unlock()
-// 	return ll
-// }
+func (l *LunchList) NumRestaurants() int {
+	if l == nil {
+		return 0
+	}
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.Countries.NumRestaurants()
+}
 
-// func (ll *LunchList) HasCountries() bool {
-// 	ll.mu.RLock()
-// 	defer ll.mu.RUnlock()
-// 	return len(ll.Countries) > 0
-// }
+func (l *LunchList) NumDishes() int {
+	if l == nil {
+		return 0
+	}
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.Countries.NumDishes()
+}
 
-// func (ll *LunchList) HasCountry(countryID string) bool {
-// 	ll.mu.RLock()
-// 	_, found := ll.Countries[countryID]
-// 	ll.mu.RUnlock()
-// 	return found
-// }
+func (l *LunchList) SetGTag(tag string) *LunchList {
+	if l == nil {
+		return nil
+	}
+	l.mu.Lock()
+	l.Gtag = tag
+	l.Countries.SetGTag(tag)
+	l.mu.Unlock()
+	return l
+}
 
-// func (ll *LunchList) HasCity(countryID, cityID string) bool {
-// 	if !ll.HasCountry(countryID) {
-// 		return false
-// 	}
-// 	return ll.GetCountryByID(countryID).HasCity(cityID)
-// }
+func (l *LunchList) Add(countries ...*Country) *LunchList {
+	if l == nil {
+		return nil
+	}
+	l.mu.Lock()
+	if l.Countries == nil {
+		l.Countries = make(CountryMap)
+	}
+	l.Countries.Add(countries...)
+	l.mu.Unlock()
+	return l
+}
 
-// func (ll *LunchList) HasSite(countryID, cityID, siteID string) bool {
-// 	if !ll.HasCity(countryID, cityID) {
-// 		return false
-// 	}
-// 	return ll.GetCityByID(countryID, cityID).HasSite(siteID)
-// }
+func (l *LunchList) Delete(ids ...string) *LunchList {
+	if l == nil {
+		return nil
+	}
+	l.mu.Lock()
+	l.Countries.Delete(ids...)
+	l.mu.Unlock()
+	return l
+}
 
-// func (ll *LunchList) HasRestaurant(countryID, cityID, siteID, restaurantID string) bool {
-// 	if !ll.HasSite(countryID, cityID, siteID) {
-// 		return false
-// 	}
-// 	return ll.GetSiteByID(countryID, cityID, siteID).HasRestaurant(restaurantID)
-// }
-
-// func (ll *LunchList) ClearCountries() *LunchList {
-// 	ll.mu.Lock()
-// 	ll.Countries = make(map[string]*Country)
-// 	ll.mu.Unlock()
-// 	return ll
-// }
-
-// func (ll *LunchList) ClearCities() *LunchList {
-// 	ll.mu.Lock()
-// 	for k := range ll.Countries {
-// 		ll.Countries[k].ClearCities()
-// 	}
-// 	ll.mu.Unlock()
-// 	return ll
-// }
-
-// func (ll *LunchList) ClearSites() *LunchList {
-// 	ll.mu.Lock()
-// 	for k := range ll.Countries {
-// 		ll.Countries[k].ClearSites()
-// 	}
-// 	ll.mu.Unlock()
-// 	return ll
-// }
-
-// func (ll *LunchList) ClearRestaurants() *LunchList {
-// 	ll.mu.Lock()
-// 	for k := range ll.Countries {
-// 		ll.Countries[k].ClearRestaurants()
-// 	}
-// 	ll.mu.Unlock()
-// 	return ll
-// }
-
-// func (ll *LunchList) ClearDishes() *LunchList {
-// 	ll.mu.Lock()
-// 	for k := range ll.Countries {
-// 		ll.Countries[k].ClearDishes()
-// 	}
-// 	ll.mu.Unlock()
-// 	return ll
-// }
-
-// func (ll *LunchList) GetCountryByID(id string) *Country {
-// 	ll.mu.RLock()
-// 	defer ll.mu.RUnlock()
-// 	return ll.Countries[id]
-// }
-
-// func (ll *LunchList) GetCityByID(countryID, cityID string) *City {
-// 	c := ll.GetCountryByID(countryID)
-// 	if c == nil {
-// 		return nil
-// 	}
-// 	return c.GetCityByID(cityID)
-// }
-
-// func (ll *LunchList) GetSiteByID(countryID, cityID, siteID string) *Site {
-// 	c := ll.GetCountryByID(countryID)
-// 	if c == nil {
-// 		return nil
-// 	}
-// 	return c.GetSiteByID(cityID, siteID)
-// }
-
-// func (ll *LunchList) GetSiteByLink(sl SiteLink) *Site {
-// 	return ll.GetSiteByID(sl.CountryID, sl.CityID, sl.SiteID)
-// }
-
-// func (ll *LunchList) GetRestaurantByID(countryID, cityID, siteID, restaurantID string) *Restaurant {
-// 	c := ll.GetCountryByID(countryID)
-// 	if c == nil {
-// 		return nil
-// 	}
-// 	return c.GetRestaurantByID(cityID, siteID, restaurantID)
-// }
-
-// func (ll *LunchList) NumCountries() int {
-// 	ll.mu.RLock()
-// 	defer ll.mu.RUnlock()
-// 	return len(ll.Countries)
-// }
-
-// func (ll *LunchList) NumCities() int {
-// 	total := 0
-// 	ll.mu.RLock()
-// 	for k := range ll.Countries {
-// 		total += ll.Countries[k].NumCities()
-// 	}
-// 	ll.mu.RUnlock()
-// 	return total
-// }
-
-// func (ll *LunchList) NumSites() int {
-// 	total := 0
-// 	ll.mu.RLock()
-// 	for k := range ll.Countries {
-// 		total += ll.Countries[k].NumSites()
-// 	}
-// 	ll.mu.RUnlock()
-// 	return total
-// }
-
-// func (ll *LunchList) NumRestaurants() int {
-// 	total := 0
-// 	ll.mu.RLock()
-// 	for k := range ll.Countries {
-// 		total += ll.Countries[k].NumRestaurants()
-// 	}
-// 	ll.mu.RUnlock()
-// 	return total
-// }
-
-// func (ll *LunchList) NumDishes() int {
-// 	total := 0
-// 	ll.mu.RLock()
-// 	for k := range ll.Countries {
-// 		total += ll.Countries[k].NumDishes()
-// 	}
-// 	ll.mu.RUnlock()
-// 	return total
-// }
+func (l *LunchList) Get(id string) *Country {
+	if l == nil {
+		return nil
+	}
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.Countries.Get(id)
+}
 
 // func (ll *LunchList) RunSiteScrapers(wg *sync.WaitGroup) {
 // 	// I _think_ we might not need to lock the whole LunchList... Should be
---
+++ lunchdata/lunchlist_test.go
@@ -0,0 +1,174 @@
+package lunchdata
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewLunchList(t *testing.T) {
+	l := NewLunchList()
+	assert.NotNil(t, l)
+	assert.NotNil(t, l.Countries)
+}
+
+func Test_LunchList_NumCountries(t *testing.T) {
+	assert.Zero(t, (*LunchList)(nil).NumCountries())
+	l := LunchList{
+		Countries: CountryMap{"1": {}},
+	}
+	assert.Equal(t, 1, l.NumCountries())
+}
+
+func Test_LunchList_NumCities(t *testing.T) {
+	assert.Zero(t, (*LunchList)(nil).NumCities())
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {
+				Cities: CityMap{"1": {}},
+			},
+		},
+	}
+	assert.Equal(t, 1, l.NumCities())
+}
+
+func Test_LunchList_NumSites(t *testing.T) {
+	assert.Zero(t, (*LunchList)(nil).NumSites())
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {
+				Cities: CityMap{
+					"1": {
+						Sites: SiteMap{"1": {}},
+					},
+				},
+			},
+		},
+	}
+	assert.Equal(t, 1, l.NumSites())
+}
+
+func Test_LunchList_NumRestaurants(t *testing.T) {
+	assert.Zero(t, (*LunchList)(nil).NumRestaurants())
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {
+				Cities: CityMap{
+					"1": {
+						Sites: SiteMap{
+							"1": {
+								Restaurants: RestaurantMap{"1": {}},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	assert.Equal(t, 1, l.NumRestaurants())
+}
+
+func Test_LunchList_NumDishes(t *testing.T) {
+	assert.Zero(t, (*LunchList)(nil).NumDishes())
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {
+				Cities: CityMap{
+					"1": {
+						Sites: SiteMap{
+							"1": {
+								Restaurants: RestaurantMap{
+									"1": {
+										Dishes: Dishes{{}},
+									}},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	assert.Equal(t, 1, l.NumDishes())
+}
+
+func Test_LunchList_SetGTag(t *testing.T) {
+	assert.Nil(t, (*LunchList)(nil).SetGTag(""))
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {
+				Cities: CityMap{
+					"1": {
+						Sites: SiteMap{
+							"1": {
+								Restaurants: RestaurantMap{
+									"1": {
+										Dishes: Dishes{{}},
+									}},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	tag := "sometag"
+	l.SetGTag(tag)
+	assert.Equal(t, tag, l.Gtag)
+	for _, country := range l.Countries {
+		assert.Equal(t, tag, country.GTag)
+		for _, city := range country.Cities {
+			assert.Equal(t, tag, city.GTag)
+			for _, s := range city.Sites {
+				assert.Equal(t, tag, s.GTag)
+				for _, r := range s.Restaurants {
+					assert.Equal(t, tag, r.GTag)
+					for _, d := range r.Dishes {
+						assert.Equal(t, tag, d.GTag)
+					}
+				}
+			}
+		}
+	}
+}
+
+func Test_LunchList_Add(t *testing.T) {
+	assert.Nil(t, (*LunchList)(nil).Add(&Country{}))
+	l := LunchList{}
+	assert.Nil(t, l.Countries)
+	ret := l.Add(nil)
+	assert.Same(t, &l, ret)
+	assert.NotNil(t, l.Countries)
+
+	c := Country{ID: "1"}
+	l.Add(&c)
+	assert.Len(t, l.Countries, 1)
+	assert.Same(t, &c, l.Countries["1"])
+}
+
+func Test_LunchList_Delete(t *testing.T) {
+	assert.Nil(t, (*LunchList)(nil).Delete(""))
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {},
+			"2": {},
+		},
+	}
+	ret := l.Delete("1")
+	assert.Same(t, &l, ret)
+	assert.Len(t, l.Countries, 1)
+	l.Delete("3")
+	assert.Len(t, l.Countries, 1)
+}
+
+func Test_LunchList_Get(t *testing.T) {
+	assert.Nil(t, (*LunchList)(nil).Get(""))
+	l := LunchList{
+		Countries: CountryMap{
+			"1": {},
+			"2": {},
+		},
+	}
+	ret := l.Get("1")
+	assert.Same(t, l.Countries["1"], ret)
+	assert.Nil(t, l.Get("3"))
+}
