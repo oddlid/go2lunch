76ff1c152fb98bd72f8422591883790ab7c46d36
Odd E. Ebbesen <oddebb@gmail.com>
2023-03-26 11:26:24 +0200

    Moved SiteScraper definition to server pakcage

--- server/scraper.go
+++ server/scraper.go
@@ -1,4 +1,6 @@
-package lunchdata
+package server
+
+import "github.com/oddlid/go2lunch/lunchdata"
 
 /*
 This file defines the interface for a scraper.
@@ -8,7 +10,7 @@ any language, as long as they provide the proper JSON to the proper URL.
 */
 
 type SiteScraper interface {
-	Scrape() (Restaurants, error)
+	Scrape() (lunchdata.Restaurants, error)
 	CountryID() string
 	CityID() string
 	SiteID() string
